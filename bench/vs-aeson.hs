6e510d23e93b43f2cc1c12fb1922e845975e60eb
Alp Mestanogullari <alpmestan@gmail.com>
2014-07-31 10:44:13 +0200

    add a benchmark against aeson with a couple of json files to bench the parsers on

---
+++ bench/vs-aeson.hs
@@ -0,0 +1,46 @@
+import Criterion.Main
+import Data.Aeson (Value)
+import Data.ByteString (ByteString)
+import qualified Data.Aeson        as Aeson
+import qualified Data.ByteString   as B
+import qualified Data.JSON.Stream  as JS
+import System.Environment (getArgs)
+import System.FilePath (takeFileName)
+
+type Label = String
+
+aeson :: ByteString -> Maybe Value
+aeson = Aeson.decodeStrict
+
+aesonStricter :: ByteString -> Maybe Value
+aesonStricter = Aeson.decodeStrict'
+
+js :: ByteString -> Maybe Value
+js = fmap fst . JS.parseValue
+
+benchOnBS :: Label -> ByteString -> Benchmark
+benchOnBS label str =
+	bgroup label
+    [ bench "aeson"          $ nf aeson str
+    , bench "aeson-stricter" $ nf aesonStricter str
+    , bench "json-streams"   $ nf js str
+    ]
+
+main :: IO ()
+main = do
+  users20    <- file "users_20.json"
+  users200   <- file "users_200.json"
+  users2000  <- file "users_2000.json"
+  users20000 <- file "users_20000.json"
+
+  defaultMain
+    [ bgroup "users"
+        [ benchOnBS "20"    users20
+        , benchOnBS "200"   users200
+        , benchOnBS "2000"  users2000
+        , benchOnBS "20000" users20000
+        ]
+    ]
+
+file :: FilePath -> IO ByteString
+file fp = B.readFile $ "bench/json_files/" ++ fp
